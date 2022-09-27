fb5873279bb4bb069d6293f37e0e1055abe523e7
Christian Fuss <christian@hackyourshack.org>
2022-09-27 13:02:56 +0000

    Added MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
+import mdx from '@astrojs/mdx';
+import m2dx from 'astro-m2dx';
+
 import { defineConfig } from 'astro/config';
 
+/** @type {import('astro-m2dx').Options} */
+const m2dxOptions = {
+  autoImports: '_autoimports.ts',
+  scanTitle: true,
+};
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  markdown: {
+    remarkPlugins: [[m2dx, m2dxOptions]],
+    extendDefaultPlugins: true,
+  },
+});
---
+++ src/pages/_autoimports.ts
@@ -0,0 +1,7 @@
+import Card from '@components/Card.astro';
+import CardGrid from '@components/CardGrid.astro';
+
+export const autoimports = {
+  CardGrid,
+  Card,
+};
