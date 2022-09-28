1dada149fba3b8167d44b34e832ec74a37f3d7f8
Christian Fuss <christian@hackyourshack.org>
2022-09-28 08:06:50 +0000

    Setup eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,39 @@
 module.exports = {
   root: true,
   env: {
+    es2021: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'turbo'],
-  plugins: ['@typescript-eslint'],
+  extends: [
+    'standard-with-typescript',
+    'prettier',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-strict',
+  ],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: false,
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+  ],
   parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json',
     sourceType: 'module',
-    ecmaVersion: 2020,
   },
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    'prettier/prettier': ['error'],
   },
-  settings: {},
 };
